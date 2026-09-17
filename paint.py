53bf7532e11fa82323708288f1e8832e0aaec8f2
BarushCerevantes <A01828910@tec.mx>
2026-09-17 15:43:44 -0600

    Le agrege el circulo

--- paint.py
+++ paint.py
@@ -38,8 +38,15 @@ def square(start, end):
 
 def circle(start, end):
     """Draw circle from start to end."""
-    pass  # TODO
+    up()
+    goto(start.x, start.y)
+    down()
+    begin_fill()
 
+    radius = ((end.x - start.x) ** 2 + (end.y - start.y) ** 2) ** 0.5
+    circle(radius)
+
+    end_fill()
 
 def rectangle(start, end):
     """Draw rectangle from start to end."""
